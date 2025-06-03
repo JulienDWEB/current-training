0da49128a28a9cc415414eadad46998695b86430
JulienDWEB <julien.sette59@gmail.com>
2025-06-03 16:24:47 +0200

    morpion

--- Progamming/JS/exo-morpion/index.js
+++ Progamming/JS/exo-morpion/index.js
@@ -1,10 +1,23 @@
-const board = document.getElementById("board");
-const cells = document.querySelectorAll(".case");
-const annonce = document.querySelector(".annonce");
- const restart = document.getElementById("restart");
+document.getElementById("startGame").addEventListener("click", function() {
+    document.getElementById("startScreen").style.display = "none"; // Cache la page d'accueil
+    document.getElementById("gameContainer").style.display = "block"; // Affiche le jeu
+});
+
+
+const board = document.getElementById("board"); // Ajout du variable board = Element qui point ID "board"
+const cells = document.querySelectorAll(".case"); // Ajout du variable cells = case de ma grille
+const annonce = document.querySelector(".annonce"); // Ajout du variable annonce = point l'element qui a la class "annonce"
+ const restart = document.getElementById("restart"); // Ajout du variable restart = qui pointe mon bouton restart 
+ const statusX = document.querySelector(".scoreStatueX") // Ajout du variable statusX = pointe vers Score X 
+ const statusY = document.querySelector(".scoreStatueO") // Ajout du variable statusX = pointe vers Score X 
+
+
 
 let gameActive      = true;   
 let currentPlayer = "x";
+
+let scoreX = 0;
+let scoreO = 0;
 
 const winCombos = [ // Combos Gagnant 
     [0,1,2],
@@ -17,6 +30,7 @@ const winCombos = [ // Combos Gagnant
     [2,4,6], 
     
 ]
+
 
 function checkWin(){
     // Some: On va exécuter une boucle sur mon tableau de combinaisons
@@ -31,49 +45,62 @@ function checkWin(){
 }
 
 function checkDraw() { // Function qui permet de verifier ce qui et ecrit dans les case 
-    return [...cells].every((clickedCase) => clickedCase.textContent !== "" ); // permet de ne pas changer le symbole de la case ! 
-}
+    return [...cells].every((clickedCase) => clickedCase.textContent !== "" ); // Permet de ne pas changer le symbole de la case ! 
+} // Permet de changer d'un tableau nodeList a ArrayList et permet d'utiliser le .every()
 
 
 function switchPlayer() { // Si x a etait ajouter a une case alors O inversement Pour O
     if(currentPlayer === "x") {
-        currentPlayer = "O";
+        currentPlayer = "O"; 
     }
     else if (currentPlayer === "O") {
-        currentPlayer = "x";
+        currentPlayer = "x"; 
     }
 }
 
-
+// Evenement au Click , Verif des case 
 
 function handleClick(event) {
- if (!gameActive) return; 
+ if (!gameActive) return;  // return si ! est false alors la partie est finie 
 
     const clickedCase = event.target;
-    if(!clickedCase.classList.contains("case") || clickedCase.textContent !== "") return;
-    clickedCase.textContent = currentPlayer; // Permet d'ajouter  du text  a la case clické
+    if(!clickedCase.classList.contains("case") || clickedCase.textContent !== "") return; // !== si il sont different de type ou de nombre alors true sinon false
+    clickedCase.textContent = currentPlayer; // Permet d'ajouter  du text  a la case clické 
 
     if (checkWin()) {
       annonce.textContent = currentPlayer +  " a gagné!";
-      annonce.style.display = "block"; // Assure que l'élément est visible
+      annonce.style.display = "block"; // Assure que l'élément est visible 
     annonce.classList.add("annonce-animation");
       gameActive = false;     
+     if (currentPlayer === "x") {  // Ajout +1 au score pour le gagnant
+        scoreX++;
+        statusX.textContent = scoreX;
+     } else {
+        scoreO++;
+        statusY.textContent = scoreO;
+     }
+
     }
 
-else if(checkDraw()) {
+else if(checkDraw()) { 
           annonce.textContent =  " Match Nul !";
-             gameActive = false;  
-}    else {
-        switchPlayer();
-    }
+          annonce.style.display = "block";
+annonce.classList.add("annonce-animation");
 
+          gameActive = false;  
+        }    else {
+            switchPlayer();
+        }
+        
 }
 
+// Réinitialiser de la partie
+
 function resetGame() {
-  cells.forEach(cell => cell.textContent = "");     // 1. Vider la grille
-  currentPlayer      = "x";                         // 2. Repasser au joueur X
-  gameActive         = true;                        // 3. Déverrouiller
-  annonce.textContent= "";                          // 4. Cacher l’annonce
+  cells.forEach(cell => cell.textContent = "");     // 1. Vider la grille 
+  currentPlayer      = "x";                         // 2. Repasser au joueur X 
+  gameActive         = true;                        // 3. Déverrouiller 
+  annonce.textContent= "";                          // 4. Cacher l’annonce 
   annonce.style.display = "none";
   annonce.classList.remove("annonce-animation");
 }
@@ -81,3 +108,19 @@ const restartBtn = document.getElementById("restart");
 restartBtn.addEventListener("click", resetGame);
 
 board.addEventListener("click" , handleClick);
+
+window.addEventListener('mousemove', (e) => {
+cursor.style.top = e.y + "px";
+cursor.style.left = e.x + "px";
+mouse1.style.top = e.y + "px";
+mouse1.style.left = e.x + "px";
+mouse2.style.top = e.y + "px";
+mouse2.style.left = e.x + "px";
+
+  
+})
+
+const mouses = document.querySelectorAll(".mouse")
+console.log(mouses);
+
+
