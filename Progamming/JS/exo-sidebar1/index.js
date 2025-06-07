f7c1a9177bac7150ac43d3836271797ea0486e93
Julien Sette <julien@MacBook-Air-de-Julien.local>
2025-06-07 13:57:11 +0200

    sideBar

---
+++ Progamming/JS/exo-sidebar1/index.js
@@ -0,0 +1,14 @@
+// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
+
+// Ranger la sidebar si on click sur le contenu principal
+
+// BONUS
+// Créer en css l'hamburger menu qui se transforme en croix
+
+// Aide
+// Priorités en CSS : !important > id > classe > baliseHtml
+const sidebar = document.getElementById('side-bar')
+btn.addEventListener('click', () => {
+    sidebar.classList.toggle('active')
+    
+})
