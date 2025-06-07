0f6ce5ca87fab664a7507161a7ec1069ed2d4106
Julien Sette <julien@MacBook-Air-de-Julien.local>
2025-06-07 11:17:04 +0200

    Scroll pupup aparition et disparition

--- Progamming/JS/exo-scroll-popup/script.js
+++ Progamming/JS/exo-scroll-popup/script.js
@@ -7,9 +7,11 @@
 // Faire apparaitre la popup quand on est en bas du site
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
+const navscroll = document.querySelector(".navbar");
+const closePopup = document.querySelector('.fa-times-circle')
+let closeDefault = true
 
 document.addEventListener("DOMContentLoaded", () => {
-  const navscroll = document.querySelector(".navbar");
   let lastScrollTop = 0;
 
   window.addEventListener("scroll", () => {
@@ -24,18 +26,31 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   
     lastScrollTop = positionScroll <= 0 ? 0 : positionScroll;
+});
+}) // Detecter si on est en bas de la page
 
-   
-   window.addEventListener('scroll', function () {
-     if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
-       const popup = document.getElementsByClassName('pop-up')[0];
- // Tu peux aussi déclencher une action ici
- if (popup) {
-   popup.style.transform = 'translateX(50px)';
-   popup.style.opacity = 1
-   
- }
-     }
-   });
-   }) // Detecter si on est en bas de la page
+// Pop-up au scroll et fermer popup
+window.addEventListener('scroll', function () {
+  if (window.scrollY + window.innerHeight >= document.body.offsetHeight && closeDefault) {
+    const popup = document.getElementsByClassName('pop-up')[0];
+    if (popup) {
+      popup.style.transform = "translateX(50px)";
+      popup.style.opacity = "1";
+      closeDefault = false;
+    }
+  }
+  
+  let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+  if (scrollValue > 0.45) {
+  imgImprovise.style.opacity = 1;
+  imgImprovise.style.transform = "none";
+  }
+  
+
+  closePopup.addEventListener('click', () => {
+    popup.style.transform = "translateX(500px)"
+    popup.style.opacity = 0;
+  })
+ 
+  
 });
