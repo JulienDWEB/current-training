bfd14a8d513a8fb1f5b6e2bc4f243151d10f7b0c
Julien Sette <julien@MacBook-Air-de-Julien.local>
2025-06-12 11:59:45 +0200

    buzzer, addeventlister ceerElement

--- Progamming/JS/exo-modifdom/js.js
+++ Progamming/JS/exo-modifdom/js.js
@@ -1,5 +1,7 @@
 const para = document.querySelector('.edit-p');
 const btn = document.querySelector('#btn');
+const navBar = document.querySelector('.liste');
+const buzzer = document.querySelector('.icon-buzz');
 
 let texteOrigin = para.textContent;
 let index = 0;
@@ -28,3 +30,51 @@ btn.addEventListener('click', () => {
         deleteLetter();
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const caseLeft = document.querySelector('.case1');
+  const caseMiddle = document.querySelector('.case2');
+const caseRight = document.querySelector('.case3')
+  caseMiddle.addEventListener('click', () => {
+    caseLeft.style.backgroundColor = "red";
+    if (caseLeft.style.backgroundColor="red") {
+        caseRight.style.backgroundColor = "blue"
+    }
+  });
+});
+function BuzzerDelete() {
+    buzzer.addEventListener('click', () => {
+        const topBuzzer = document.querySelector('.top-buzzer1');
+        const Entete = document.querySelector('.entete')
+        topBuzzer.style.transform = "translateY(10px)";
+        setTimeout(() => {
+            topBuzzer.style.transform = "translateY(0)";
+        }, 500);
+
+        const newLn = document.createElement('li');
+        newLn.textContent = "Nouvel Élément !";
+        Entete.appendChild(newLn);
+        const lastChild = Entete.lastElementChild;
+if (lastChild) {
+    Entete.removeChild(lastChild);
+}
+    });
+}
+function BuzzerActive() {
+    buzzer.addEventListener('click', () => {
+        const topBuzzer = document.querySelector('.top-buzzer');
+        console.log(topBuzzer);
+        
+        const Entete = document.querySelector('.entete')
+        topBuzzer.style.transform = "translateY(10px)";
+        setTimeout(() => {
+            topBuzzer.style.transform = "translateY(0)";
+        }, 500);
+
+        const newLn = document.createElement('li');
+        newLn.textContent = "Nouvel Élément !";
+        Entete.appendChild(newLn);
+        
+    });
+}
+BuzzerActive();
