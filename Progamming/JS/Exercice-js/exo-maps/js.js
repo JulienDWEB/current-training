662fab74a5c12a8c057564fcdcd37a695ce82303
Julien Sette <julien@MacBook-Air-de-Julien.local>
2025-06-20 13:04:38 +0200

    Carte Interactive

--- Progamming/JS/Exercice-js/exo-maps/js.js
+++ Progamming/JS/Exercice-js/exo-maps/js.js
@@ -1,12 +1,52 @@
-fetch('https://api.maptiler.com/maps/01978879-bea3-75fc-835a-7066a4307b7f/?key=JRheCvRpTzAQNg2S5SvL#5.8/47.13460/8.75150') 
-    .then(Response => Response.json())
-    .then(data => {
-      console.log(data);
-      
-    })
-    const map = L.map('map').setView([47.13460, 8.75150], 6);
+let map = L.map('map').setView([100, -0.39], 13);
+    
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-L.tileLayer('https://api.maptiler.com/maps/01978879-bea3-75fc-835a-7066a4307b7f/256/{z}/{x}/{y}.png?key=JRheCvRpTzAQNg2S5SvL', {
-    attribution: '&copy; <a href="https://www.maptiler.com/copyright/">MapTiler</a>',
-    tileSize: 256
-}).addTo(map);  
+console.log(map);
+
+
+//api goweather meteo
+// recuperer la longitude et l'atitude
+// recuperer la ville quand on click sur la carte
+
+    map.addEventListener('click', (e) => {
+        let lat = e.latlng.lat.toFixed(6);
+        let lon = e.latlng.lng.toFixed(6);
+        console.log(`l'atitude = ${lat}`);
+        console.log(`longitude = ${lon}`);
+        fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                const pays = document.createElement('p');
+                const city = document.createElement('p');
+                const infoPosition = document.querySelector('.info-position');
+                pays.textContent = `Pays: ${data.address?.country || "Inconnu"}`;
+                city.textContent = `Ville: ${data.address?.city || "Inconnu"}`;
+                infoPosition.appendChild(pays);
+                infoPosition.appendChild(city);
+    const cityName = data.address.city;
+                
+               /*  const cityName = data.address.county; */
+fetch(`https://goweather.xyz/weather/${cityName}`) 
+.then(response => response.json())
+.then(data => { 
+const meteoInfo = document.createElement('p');
+infoPosition.appendChild(meteoInfo);
+meteoInfo.textContent = `Météo: ${data.temperature}, ${data.wind}`;
+console.log(data);
+
+});
+                                
+
+
+})
+
+            });
+
+
+
+
